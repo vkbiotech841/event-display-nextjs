287d02026da2861bf6f62522619604b5f3a7acf7
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-03-12 12:30:49 +0530

    basic folder structure  added

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const filteredEventPage = () => {
+    return (
+        <div>
+            <h1>Filtered events</h1>
+        </div>
+    )
+}
+
+export default filteredEventPage
