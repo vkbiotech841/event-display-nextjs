5271a8f948cac20d7be8531f9f15b08e11e25885
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-03-13 15:24:53 +0530

    event ui and icon added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,10 @@
 import Link from 'next/link';
+import classes from './event-item.module.css';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+
 
 const EventItem = (props) => {
     const { title, image, date, location, id } = props;
@@ -15,21 +21,30 @@ const EventItem = (props) => {
 
 
     return (
-        <li>
+        <li className={classes.item}>
             <img src={'/' + image} alt={title} />
-            <div>
-                <div>
+            <div className={classes.content}>
+                <div className={classes.summary}>
                     <h2>{title}</h2>
-                    <div>
+                    <div className={classes.date}>
+                        <DateIcon />
                         <time>{humanReadableDate}</time>
                     </div>
-                    <div>
+                    <div className={classes.address}>
+                        <AddressIcon />
                         <address>{formattedAddress}</address>
                     </div>
                 </div>
 
-                <div>
-                    <Link href={exploreLink}>Explore Event</Link>
+                <div className={classes.actions}>
+                    <Button link={exploreLink} >
+                        <span>
+                            Explore Event
+                        </span>
+                        <span className={classes.icon}>
+                            <ArrowRightIcon />
+                        </span>
+                    </Button>
                 </div>
             </div>
         </li>
