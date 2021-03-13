7d7c252aedf86a4b1f308f6ebd58a54bdec5fe13
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-03-13 18:10:07 +0530

    adding slug and filtering events done

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,24 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/events-search';
 
 const AllEventsPage = () => {
+    const router = useRouter();
     const events = getAllEvents();
 
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    };
+
     return (
-        <div>
+        <Fragment>
+            <EventSearch onSearch={findEventsHandler} />
             <EventList items={events} />
-        </div>
+        </Fragment>
     )
 }
 
