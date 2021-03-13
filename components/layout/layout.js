d12c36cc67d1668ff8c8ec1ac8e1489e1aac341b
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-03-13 16:20:22 +0530

    event details by id added

---
+++ components/layout/layout.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import MainHeader from "./main-header";
+
+const Layout = (props) => {
+    return (
+        <Fragment>
+            <MainHeader />
+            <header></header>
+            <main>
+                {props.children}
+            </main>
+        </Fragment>
+    )
+}
+
+export default Layout;
